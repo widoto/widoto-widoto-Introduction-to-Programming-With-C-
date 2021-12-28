c2bc66eabfd71f7a492001ab3d2a32000465c302
widoto <minhaekim44@gmail.com>
2021-12-28 10:54:22 +0900

    first commit...

---
+++ WorkbookAnswers/WarmingUp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to C++" << endl;
+	cout << "Welcome to Computer Science" << endl;
+	cout << "Programming is fun" << endl;
+	return 0;
+}
