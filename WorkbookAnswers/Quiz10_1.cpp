d4629ea7286675436f1347df1d73fed4d81636db
widoto <minhaekim44@gmail.com>
2022-02-17 11:52:48 +0900

    2022/02/17

---
+++ WorkbookAnswers/Quiz10_1.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1("I have a dream");
+	string s2("Computer Programming");
+
+	//cout << s1.append(s2) << endl;
+	//cout << s1.append(s2, 9, 7) << endl;
+	//cout << s1.append("New", 3) << endl;
+	//cout << s1.append(3, 'N') << endl;
+	//cout << s1.assign(3, 'N') << endl;
+	//cout << s1.assign(s2, 9, 7) << endl;
+	//cout << s1.assign("NEWNEW", 3) << endl;
+	//cout << s1.assign(3, 'N') << endl;
+	//cout << s1.at(0) << endl;
+	//cout << s1.length() << endl;
+	//cout << s1.size() << endl;
+	//cout << s1.capacity() << endl;
+	//cout << s1.erase(1, 2) << endl;
+	//cout << s1.compare(s2) << endl;
+	//cout << s1.compare(0, 10, s2) << endl;
+	//cout << s1.c_str() << endl;
+	//cout << s1.substr(4, 8) << endl;
+	//cout << s1.substr(4) << endl;
+	//cout << s1.find('A') << endl;
+	//cout << s1.find('a', 9) << endl;
+	//cout << s1.replace(2, 4, "New") << endl;
+	//cout << s1.insert(4, "NEW") << endl;
+	//cout << s1.insert(6, 8, 'N') << endl;
+	//cout << s1.empty();
+
+
+
+
+	return 0;
+}
