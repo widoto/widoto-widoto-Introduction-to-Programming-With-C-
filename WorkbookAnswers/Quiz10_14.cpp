1c81351100dedddbe1a2fea552fc5cdabd126ac6
widoto <minhaekim44@gmail.com>
2022-02-23 09:44:47 +0900

    2022/02/23

---
+++ WorkbookAnswers/Quiz10_14.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string Cities[] = { "Atlanta", "Dallas", "Savannah" };
+	cout << Cities[0] << endl;
+	cout << Cities[1] << endl;
+}
